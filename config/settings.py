64b18ba4e7d26c824eaa7193b17e2c73195ea6d4
TC <xiii33iiix@gmail.com>
2021-05-03 21:29:53 -0400

    new log files

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-php7rus+1hx635v2t@pxk1bu!&zd#=-t^(($m%$*-l*q*-627n
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [".herokuapp.com", "localhost", "127.0.0.1"]
 
 
 # Application definition
